dff932bb748e2340a5988e96692027ad35fd95dd
Harley Stephen A. Soldao <157875075+Haxlsy@users.noreply.github.com>
2025-05-31 01:03:51 +0800

    added change password form

---
+++ HospitalManagementSystem/View/ChangePasswordcs.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace HospitalManagementSystem.View
+{
+    partial class ChangePasswordcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.WindowFrame;
+            label2.Location = new Point(12, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 20);
+            label2.TabIndex = 8;
+            label2.Text = "CHANGE PASSWORD";
+            // 
+            // ChangePasswordcs
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(531, 454);
+            Controls.Add(label2);
+            MaximizeBox = false;
+            Name = "ChangePasswordcs";
+            Text = "ChangePasswordcs";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+    }
+}
